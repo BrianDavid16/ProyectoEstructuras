625e9faba1a582ee63dea08c59ba035b6700b550
Brian Hortua <42702087+BrianDavid16@users.noreply.github.com>
2019-02-27 10:09:43 -0500

    V1.3
    CHANGELOG
    ------------------------------------------
    VERSION 1.3
    -Lectura de archivos correcta
    -Punto 1 añadido
    ------------------------------------------
    VERSION 1.2
    Mejorado lectura de archivos
    ----------------------------------------------------------------------------
    VERSIÓN 1.1
    Añadido throw exception cuando no pueda leer el archivo "prueba.txt"
    ----------------------------------------------------------------------------
    VERSION 1.0
    Versión inicial

--- volumen.h
+++ volumen.h
@@ -8,7 +8,13 @@ public:
     volumen();
     void cargar_volumen(unsigned int n,std::vector<imagen> );
     void proyeccion2D (unsigned int pos);//toma las imagenes ya cargadas, preguntarle al cucho porque no entiendo
-
+    void setNombre_base(const std::string&);
+    void setTam(const int&);
+    void setMisImagenes(const std::vector<imagen>&);
+    std::string getNombre_base()const;
+    unsigned int getTam()const;
+    std::vector<imagen> getMisImagenes()const;
+    void cargar_volumen(imagen&);
 protected:
     std::string nombre_base;
     unsigned int tam; //maximo 99
