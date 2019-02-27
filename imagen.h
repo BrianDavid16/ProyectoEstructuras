36b3a03fc6c71d0a7d097fd8247e48e3592d8747
Brian Hortua <42702087+BrianDavid16@users.noreply.github.com>
2019-02-27 10:04:55 -0500

    V 1.3
    CHANGELOG
    ------------------------------------------
    VERSION 1.3
    -Lectura de archivos correcta
    -Punto 1 añadido
    ------------------------------------------
    VERSION 1.2
    Mejorado lectura de archivos
    ----------------------------------------------------------------------------
    VERSIÓN 1.1
    Añadido throw exception cuando no pueda leer el archivo "prueba.txt"
    ----------------------------------------------------------------------------
    VERSION 1.0
    Versión inicial

--- imagen.h
+++ imagen.h
@@ -3,6 +3,11 @@
 
 #include <string>
 #include <vector>
+#include <fstream>
+#include <cstring>
+#include <sstream>
+#include <string>
+#include <iostream>
 #include "punto.h"
 
 class imagen {
@@ -11,13 +16,17 @@ public:
     void setW(const int&);
     void setH(const int&);
     void setM(const int&);
+    void setNombre(const std::string&);
     unsigned int getM()const;
     unsigned int getW()const;
     unsigned int getH()const;
+    std::string getNombre()const;
     void setMyMatrix(const std::vector<std::vector<punto>>&);
     std::vector<std::vector<punto>> getMyMatrix()const;
-
-
+    void resizeVectors();
+    void leerArchivo();
+    int charToInt(char* h);
+    void printImage();
 protected:
     std::string nombre; //nombre base y dos numeros de posicion
     unsigned int W;
