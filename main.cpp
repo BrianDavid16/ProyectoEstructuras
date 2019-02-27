b64f4daaaa045156f74660fc2306321c454de1d1
Brian Hortua <42702087+BrianDavid16@users.noreply.github.com>
2019-02-27 10:06:15 -0500

    V1.3
    CHANGELOG
    ------------------------------------------
    VERSION 1.3
    -Lectura de archivos correcta
    -Punto 1 añadido
    ------------------------------------------
    VERSION 1.2
    Mejorado lectura de archivos
    ----------------------------------------------------------------------------
    VERSIÓN 1.1
    Añadido throw exception cuando no pueda leer el archivo "prueba.txt"
    ----------------------------------------------------------------------------
    VERSION 1.0
    Versión inicial

--- main.cpp
+++ main.cpp
@@ -1,109 +1,48 @@
 #include <iostream>
-#include <cstring>
-#include <sstream>
-#include <fstream>
 #include <string>
+#include <cstring>
+#include <stdio.h>
+#include <string.h>
 
 #include "punto.h"
 #include "imagen.h"
+#include "volumen.h"
+
 int main(){
+  imagen ph;
+  volumen vol;
+  char comando[100];
+  char* pch,*cargar_volumen,*n_base,*aux_n;
+  int contador=0;
+  std::cout<<"ingrese: ";
+  std::cin.getline(comando,100);
 
-}
-int charToInt(char* h){
-  int r;
-  std::istringstream conv(h);
-  conv >>r;
-  return r;
-}
-void tokenizar(const std::string &l, imagen &ph, const int &j){
-    int count = 0;
-    punto p;
-    std::string linea=l;
-    char *linea_aux = new char[linea.length() + 1];
-    strcpy(linea_aux, linea.c_str());
-    char* actual;
-    char* bandera;
-    std::vector<std::vector<punto>> aux_vector;
-    int rgb,i=0;
-    while (bandera!=NULL) {
-      if(linea_aux[0]=='#'){
-        //comentario, aun no sabemos si lo vamos a guardar o que pex
+  pch = strtok (comando," ");
+  while (pch != NULL)
+  {
+    if(contador==0){
+      cargar_volumen=pch;
+    }
+    if(contador==1){
+      n_base=pch;
+      vol.setNombre_base(n_base);
+    }
+    if(contador==2){
+      aux_n=pch;
+      int tam;
+      std::istringstream conv(aux_n);
+      conv >>tam;
+      if(tam<0){
+        throw std::runtime_error("La imagen no ha podido cargarse->El tamaño debe ser mayor a 0");
       }
-      else{
-        aux_vector=ph.getMyMatrix();
-        actual=strtok(linea_aux," ");
-        rgb=charToInt(actual);
-        if((rgb>ph.getM())&&(rgb<0)){
-          throw std::runtime_error("Valor invalido para r");
-        }
-        p.setR(rgb);
-        actual=strtok(NULL," ");
-        rgb=charToInt(actual);
-        if((rgb>ph.getM())&&(rgb<0)){
-          throw std::runtime_error("Valor invalido para g");
-        }
-        p.setG(rgb);
-        actual=strtok(NULL," ");
-        rgb=charToInt(actual);
-        if((rgb>ph.getM())&&(rgb<0)){
-          throw std::runtime_error("Valor invalido para b");
-        }
-        p.setB(rgb);
-      }
-      bandera=strtok(linea_aux,"\t");
-      i++;
-      aux_vector[j][i]=p;
-      ph.setMyMatrix(aux_vector);
+      vol.setTam(tam);
     }
-    delete [] linea_aux;
-}
-bool leerArchivo()
-{
-    int contador = 0,M,W,H,j=0;
-    char* w;
-    char* h;
-    imagen ph;
-    std::string m,coment,linea,formato;
-    try
-    {
-    std::ifstream myfile("prueba.txt");
-    if (myfile.is_open()) {
-        getline(myfile, formato);
-        getline(myfile, coment);
-        if(coment[0]=='#'){
-        //comentario, aun no sabemos si lo vamos a guardar o que pex
-          getline(myfile, coment);
-        }
-        char *coment_aux = new char[coment.length() + 1];
-        strcpy(coment_aux, coment.c_str());
-        w=strtok(coment_aux," ");
-        W=charToInt(w);
-        ph.setW(W);
-        h=strtok(NULL," ");
-        H=charToInt(h);
-        ph.setH(H);
-        getline(myfile, m);
-        M=std::stoi(m);
-        if((M>255)&&(M<0)){
-            throw std::runtime_error("Valor invalido para M");
-        }
-        ph.setM(M);
-        while (getline(myfile, linea)) {
-            tokenizar(linea,ph,j);
-            j++;
-        }
-        delete [] coment_aux;
-        return true;
-    }else{
-      throw 1;
-    }
-	catch (int A)
-    {
-        if (A==1)
-        {
-            std::cout << "Archivo no leido, compruebe nombre y/o ubicacion" << std::endl;
-        }
-    }
-}
-
+    pch = strtok (NULL, " ");
+    contador++;
+  }
+  if(strcmp(cargar_volumen,"cargar_volumen")==0){
+    vol.cargar_volumen(ph);
+  }else{
+    throw std::runtime_error("Comando invalido");
+  }
 }
