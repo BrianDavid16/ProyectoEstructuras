1b01c17d26684f8fb3c1f85dafdb0e6de9a64fb5
Brian Hortua <42702087+BrianDavid16@users.noreply.github.com>
2019-02-27 10:08:38 -0500

    V1.3
    CHANGELOG
    ------------------------------------------
    VERSION 1.3
    -Lectura de archivos correcta
    -Punto 1 añadido
    ------------------------------------------
    VERSION 1.2
    Mejorado lectura de archivos
    ----------------------------------------------------------------------------
    VERSIÓN 1.1
    Añadido throw exception cuando no pueda leer el archivo "prueba.txt"
    ----------------------------------------------------------------------------
    VERSION 1.0
    Versión inicial

--- volumen.cxx
+++ volumen.cxx
@@ -1,6 +1,41 @@
 #include "volumen.h"
+#include <iostream>
+#include <stdlib.h>
+#include <sstream>
+#include <string>
 volumen::volumen(){}
-void volumen::cargar_volumen(unsigned int n,std::vector<imagen> i){
+void volumen::cargar_volumen(imagen &ph){
+  this->MisImagenes.resize(this->tam);
+  for(int i=0;i<this->tam;i++){
+    ph.leerArchivo();
+    ph.printImage();
+    this->MisImagenes[i]=ph;
+    std::string nombre_completo;
+    std::string format=".ppm";
+    std::string pos = std::to_string(i);
+    nombre_completo=this->nombre_base+pos+format;
+    this->MisImagenes[i].setNombre(nombre_completo);
+    //std::cout<<this->MisImagenes[i].getNombre();
+    std::cin.get();
+  }
 }
 void volumen::proyeccion2D (unsigned int pos){
 }//toma las imagenes ya cargadas, preguntarle al cucho porque no entiendo
+void volumen::setNombre_base(const std::string&l){
+  this->nombre_base=l;
+}
+void volumen::setTam(const int&t){
+  this->tam=t;
+}
+void volumen::setMisImagenes(const std::vector<imagen>&m){
+  this->MisImagenes=m;
+}
+std::string volumen::getNombre_base()const{
+  return this->nombre_base;
+}
+unsigned int volumen::getTam()const{
+  return this->tam;
+}
+std::vector<imagen> volumen::getMisImagenes()const{
+  return this->MisImagenes;
+}
